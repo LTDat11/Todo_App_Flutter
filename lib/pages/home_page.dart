1ff127df1425e2144287fce434d312abc750fb16
dat lu <Ludat2410@gmail.com>
2025-02-02 12:42:58 +0700

    show list todo

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  List toDoList = [
+    ['Learn Flutter', true],
+    ['Learn Firebase', true],
+    ['Learn Backend', false],
+    ['Buy Code', false]
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Text(
+          'TODO APP',
+        ),
+        backgroundColor: Colors.black,
+        foregroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: toDoList.isEmpty
+          ? const Center(
+              child: Text('Empty'),
+            )
+          : ListView.builder(
+              itemCount: toDoList.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading: toDoList[index][1]
+                      ? const Icon(Icons.check)
+                      : const Icon(Icons.radio_button_unchecked),
+                  title: Text(toDoList[index][0]),
+                );
+              },
+            ),
+    );
+  }
+}
